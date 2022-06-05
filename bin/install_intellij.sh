280b2d030041bbcf4e977963a54372693a652e23
rumaria <mfalah.rumaria@bankneo.co.id>
2022-06-05 21:00:13 +0700

    add in install idea & pycharm script: 1. config 2. download if tarball is not exist

--- bin/install_intellij.sh
+++ bin/install_intellij.sh
@@ -5,6 +5,7 @@
 # idea.sh
 # from anywhere
 
+base_dir=$HOME/rmr_code_repo/rumaria-linux-script
 path_linux_app=/mnt/d/linux
 path_download=$HOME/Downloads
 
@@ -13,12 +14,32 @@ file_tarball=$(ls idea*.tar.gz)
 
 if [ -z "$file_tarball" ]; then
   echo "idea's tarball does not exist"
-  exit 1
+  download_url=$(grep -E "url.idea.dl" $base_dir/config | awk '{print $3}')
+  echo "please download first, input url below"
+  echo "(for reference purpose, pattern will be like $download_url)"
+  read -p "input : " download_url
+  wget "$download_url"
+  if [ $? != 0 ]; then
+    echo "trying to open the download page"
+    echo "hopefully it will prompt the browser to download, but copy the url below because we want to download here instead for automation purpose of the next step"
+    firefox $(grep -E "url.idea.page" $base_dir/config | awk '{print $3}')
+    read -p "input : " download_url
+    wget "$download_url"
+  fi
+  file_tarball=$(ls idea*.tar.gz)
+  if [ -z "$file_tarball" ]; then
+    echo "idea's tarball does not exist"
+    exit 1
+  fi
+  # save new download url
+  sed "s/url.idea.dl = .*/url.idea.dl = $download_url/" "$base_dir/config" >"$base_dir/config"
 fi
 
 # close running app
-main_process=($(ps -ef | grep --max-count=1 idea.*/jbr/bin/java))
-kill -9 ${main_process[1]} # index 1 is pid
+main_pid=$(ps -ef |
+  grep --max-count=1 idea.*/jbr/bin/java |
+  awk '{print $2}') # field 2 is pid
+kill -9 $main_pid
 
 cd $path_linux_app
 cur_idea_dir=$(ls -d idea*)
@@ -26,6 +47,11 @@ echo "current idea dir : " $cur_idea_dir
 # remove existing dir app if exist
 if [ -n "$cur_idea_dir" ]; then
   rm -rf "$cur_idea_dir"
+  while [ $? != 0 ]; do
+    echo "trying to remove $cur_idea_dir again"
+    rm -rf "$cur_idea_dir"
+  done
+  echo "successfully remove $cur_idea_dir"
 fi
 
 # extract tarball
@@ -33,12 +59,10 @@ tar xvf $path_download/$file_tarball
 
 path_script=$(ls idea*/bin/idea.sh)
 
-# back to home
-cd
 # create symbolic link
-ln --force -s $path_linux_app/$path_script bin/idea.sh
+ln --force -s $path_linux_app/$path_script $base_dir/bin/idea.sh
 
-trash-put $file_tarball
+trash-put $path_download/$file_tarball
 
 echo "idea updated!"
 echo -e "please run\n $ idea.sh"
--- bin/install_pycharm.sh
+++ bin/install_pycharm.sh
@@ -5,6 +5,7 @@
 # pycharm.sh
 # from anywhere
 
+base_dir=$HOME/rmr_code_repo/rumaria-linux-script
 path_linux_app=/mnt/d/linux
 path_download=$HOME/Downloads
 
@@ -13,12 +14,32 @@ file_tarball=$(ls pycharm*.tar.gz)
 
 if [ -z "$file_tarball" ]; then
   echo "pycharm's tarball does not exist"
-  exit 1
+  download_url=$(grep -E "url.pycharm.dl" $base_dir/config | awk '{print $3}')
+  echo "please download first, input url below"
+  echo "(for reference purpose, pattern will be like $download_url)"
+  read -p "input : " download_url
+  wget "$download_url"
+  if [ $? != 0 ]; then
+    echo "trying to open the download page"
+    echo "hopefully it will prompt the browser to download, but copy the url below because we want to download here instead for automation purpose of the next step"
+    firefox $(grep -E "url.pycharm.page" $base_dir/config | awk '{print $3}')
+    read -p "input : " download_url
+    wget "$download_url"
+  fi
+  file_tarball=$(ls pycharm*.tar.gz)
+  if [ -z "$file_tarball" ]; then
+    echo "pycharm's tarball does not exist"
+    exit 1
+  fi
+  # save new download url
+  sed "s/url.pycharm.dl = .*/url.pycharm.dl = $download_url/" "$base_dir/config" >"$base_dir/config"
 fi
 
 # close running app
-main_process=($(ps -ef | grep --max-count=1 pycharm.*/jbr/bin/java))
-kill -9 ${main_process[1]} # index 1 is pid
+main_pid=$(ps -ef |
+  grep --max-count=1 pycharm.*/jbr/bin/java |
+  awk '{print $2}') # field 2 is pid
+kill -9 $main_pid
 
 cd $path_linux_app
 cur_pycharm_dir=$(ls -d pycharm*)
@@ -26,6 +47,11 @@ echo "current pycharm dir : " $cur_pycharm_dir
 # remove existing dir app if exist
 if [ -n "$cur_pycharm_dir" ]; then
   rm -rf "$cur_pycharm_dir"
+  while [ $? != 0 ]; do
+    echo "trying to remove $cur_pycharm_dir again"
+    rm -rf "$cur_pycharm_dir"
+  done
+  echo "successfully remove $cur_pycharm_dir"
 fi
 
 # extract tarball
@@ -33,12 +59,10 @@ tar xvf $path_download/$file_tarball
 
 path_script=$(ls pycharm*/bin/pycharm.sh)
 
-# back to home
-cd
 # create symbolic link
-ln --force -s $path_linux_app/$path_script bin/pycharm.sh
+ln --force -s $path_linux_app/$path_script $base_dir/bin/pycharm.sh
 
-trash-put $file_tarball
+trash-put $path_download/$file_tarball
 
 echo "pycharm updated!"
 echo -e "please run\n $ pycharm.sh"
