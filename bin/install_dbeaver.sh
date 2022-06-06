104ac31a9c0a91a34335b3791fc179685fa9d969
rumaria <mfalah.rumaria@bankneo.co.id>
2022-06-06 17:23:14 +0700

    dbeaver: prompt download tar

--- bin/install_dbeaver.sh
+++ bin/install_dbeaver.sh
@@ -5,6 +5,7 @@
 # dbeaver
 # from anywhere
 
+base_dir=$HOME/rmr_code_repo/rumaria-linux-script
 path_linux_app=/mnt/d/linux
 path_download=$HOME/Downloads
 
@@ -12,20 +13,42 @@ cd $path_download
 file_tarball=$(ls dbeaver*.tar.gz)
 
 if [ -z "$file_tarball" ]; then
-  echo "dbeaver's tarball does not exist"
-  exit 1
+	echo "dbeaver's tarball does not exist"
+	download_url=$(grep -E "url.dbeaver.dl" $base_dir/config | awk '{print $3}')
+	echo "please download first, input url below"
+	echo "(for reference purpose, pattern will likely be like $download_url)"
+	read -p "input : " download_url
+	wget "$download_url"
+	if [ $? != 0 ]; then
+		echo "trying to open the download page, hopefully it will prompt the browser to download"
+		echo "but copy the url below because we want to download here instead for automation purpose of the next step"
+		firefox $(grep -E "url.dbeaver.page" $base_dir/config | awk '{print $3}')
+		read -p "input : " download_url
+		wget "$download_url"
+	fi
+	file_tarball=$(ls dbeaver*.tar.gz)
+	if [ -z "$file_tarball" ]; then
+		echo "dbeaver's tarball does not exist"
+		exit 1
+	fi
+	# save new download url
+	sed "s/url.dbeaver.dl = .*/url.dbeaver.dl = $download_url/" "$base_dir/config" > "$base_dir/config1"
+	mv "$base_dir/config1" "$base_dir/config"
 fi
 
 # close running app
-main_process=($(ps -ef | grep --max-count=1 dbeaver/jre/bin/java))
-kill -9 ${main_process[1]} # index 1 is pid
+main_pid=$(ps -ef \
+	| grep --max-count=1 dbeaver/jre/bin/java \
+	| awk '{print $2}') # field 2 is pid
+kill -9 $main_pid
 
 cd $path_linux_app
 cur_dbeaver_dir=$(ls -d dbeaver*)
 echo "current dbeaver dir : " $cur_dbeaver_dir
 # remove existing dir app if exist
 if [ -n "$cur_dbeaver_dir" ]; then
-  rm -rf "$cur_dbeaver_dir"
+	trash-put "$cur_dbeaver_dir"
+	echo "move $cur_dbeaver_dir to trash"
 fi
 
 # extract tarball
@@ -33,12 +56,10 @@ tar xvf $path_download/$file_tarball
 
 path_script=$(ls dbeaver*/dbeaver)
 
-# back to home
-cd
 # create symbolic link
-ln --force -s $path_linux_app/$path_script bin/dbeaver
+ln --force -s $path_linux_app/$path_script $base_dir/bin/dbeaver
 
-trash-put $file_tarball
+trash-put $path_download/$file_tarball
 
 echo "dbeaver updated!"
-echo -e "please run\n $ dbeaver"
+echo -e "please run \n$ dbeaver"
