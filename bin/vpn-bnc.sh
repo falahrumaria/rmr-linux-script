e31a76b2575c07ce7f3347423ee602af2944371c
rumaria <rumariafalah08@gmail.com>
2022-07-01 11:01:33 +0700

    sleep 1s before trying to turn on vpn

--- bin/vpn-bnc.sh
+++ bin/vpn-bnc.sh
@@ -2,20 +2,22 @@
 
 # bnc specific script
 
-if [ -z $1 ]; then
+if ! test "$1"; then
 	if nmcli --ask con down BNC; then
 		exit
 	fi
 	nmcli --ask con down BNC_1
+	sleep 1
 	nmcli --ask con up BNC
 	exit
 fi
 
-if [ $1 = "1" ]; then
+if test "$1" = "1"; then
 	if nmcli --ask con down BNC_1; then
 		exit
 	fi
 	nmcli --ask con down BNC
+	sleep 1
 	nmcli --ask con up BNC_1
 	exit
 fi
