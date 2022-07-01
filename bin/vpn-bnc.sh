226b1c1a598c749875da8e33df40c2a236e65111
rumaria <rumariafalah08@gmail.com>
2022-07-02 05:13:46 +0700

    simplify and discard error output

--- bin/vpn-bnc.sh
+++ bin/vpn-bnc.sh
@@ -3,23 +3,15 @@
 # bnc specific script
 
 if ! test "$1"; then
-	if nmcli --ask con down BNC; then
-		exit
-	fi
-	nmcli --ask con down BNC_1
-	sleep 1
-	nmcli --ask con up BNC
-	exit
+    ! nmcli --ask con down BNC 2> /dev/null || exit
+    ! nmcli --ask con down BNC_1 2> /dev/null || sleep 1
+    nmcli --ask con up BNC
+    exit
 fi
 
-if test "$1" = "1"; then
-	if nmcli --ask con down BNC_1; then
-		exit
-	fi
-	nmcli --ask con down BNC
-	sleep 1
-	nmcli --ask con up BNC_1
-	exit
-fi
+! nmcli --ask con down BNC_1 2> /dev/null || exit
+! nmcli --ask con down BNC 2> /dev/null || sleep 1
+nmcli --ask con up BNC_1
+exit
 
 echo "wrong argument!"
